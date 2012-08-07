ed2127d30c3486409094c3b3134ac93b0775ce04
Matt Wiggins <matt.wiggins@legworkstudio.com>
2012-08-07 11:17:41 -0600

    Default rails setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Legwork2012::Application.config.secret_token = '59329685a1ceda5bd33cdfa61b8a2cf05901c42bfd993677166bc3cebd757063ced5f8b79a194b092520f793cff86250d5c6799c6a3b270db9d5bed9f19a6c94'
